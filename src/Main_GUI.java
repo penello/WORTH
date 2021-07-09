ac73c19bf4e6226ca07686e94deee57dfdf5235a
penello <dada_cr@libero.it>
2021-07-09 18:27:09 +0200

    updates

--- src/Main_GUI.java
+++ src/Main_GUI.java
@@ -1,14 +1,26 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main_GUI {
-    private JButton button1;
-    private JButton button2;
     private JPanel panel_main;
+    private JButton SIGNINButton;
+    private JButton LOGINButton;
+    private JLabel WORTHLabel;
+    private JTextField Username;
+    private JTextField Password;
 
     public Main_GUI(JFrame frame){
         frame.setContentPane(panel_main);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        LOGINButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String username = Username.getText();
+                String password = Password.getText();
+            }
+        });
     }
 }
